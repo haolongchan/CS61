fbde4b7a5460da2e356f85718ce391f553c2f753
Haolong <chl123.chenhaolong@gmail.com>
2024-10-13 22:15:11 -0400

    done wrongly

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,16 +5,11 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n == 128)
-        {
-            return 1;
+        if (n % 2 == 0){
+            return n / 2;
         }
-        else if (n == 5)
-        {
+        else{
             return n * 3 + 1;
-        }
-        else {
-            return n * 2;
         }
     }
 
