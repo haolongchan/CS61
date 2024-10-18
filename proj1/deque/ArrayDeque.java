67aa42567aa673c13d95ffa6ed9b24fb10e317fa
Haolong <chl123.chenhaolong@gmail.com>
2024-10-18 02:07:13 -0400

    fix next()

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -43,7 +43,7 @@ public class ArrayDeque<Item> implements Deque<Item> {
 
 //        @Override
         public boolean hasNext() {
-            return current != null;
+            return index < size;
         }
 
 //        @Override
